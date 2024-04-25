1a6954764cdaf2705940b6f032626dcd678fcdb8
Sergio <sergiohervas9@gmail.com>
2024-04-25 23:48:02 +0200

    descripcion

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 export default function App() {
   return (
-    <section className="flex m-auto justify-center my-8">
-      <h1 className="mb-4 text-3xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl">Encuesta de Cumpleaños</h1>
-      
+    <section className="">
+      <h1 className="flex m-auto justify-center my-8 mb-4 text-3xl font-extrabold text-gray-900 md:text-5xl lg:text-6xl">Encuesta de Cumpleaños</h1>
+      <p className="flex m-auto justify-center  text-center mx-10 mb-6 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 xl:px-48">Selecciona para que personas vas a realizar puesta de dinero para regalos</p>
     </section>
   )
 }
